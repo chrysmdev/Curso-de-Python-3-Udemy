cad68206d795a5772cd964977a6971e198546540
Chrystian Menezes <chrys.dev21@gmail.com>
2025-06-04 17:32:00 -0300

    Create aula7.py

---
+++ aula7.py
@@ -0,0 +1,20 @@
+# Váriaveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar números e uderline _.
+# O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+# nome_completo = "Chrystian Menezes Nascimento"
+# soma_dois_mais_dois = 2 + 2
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = "Chrystian"
+idade = 21
+maior_de_idade = idade >= 18
+print("Nome:", nome, "Idade:", idade, " anos")
+
+# if maior_de_idade:
+#     maior_de_idade = "Sim, ele é de maior!"
+# else:
+#     maior_de_idade = "Não, ele não é de maior!"
+
+print("É de maior?", maior_de_idade)
