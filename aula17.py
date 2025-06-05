f96f2a3e596565bafd788b70a722ed2c94d93af1
Chrystian Menezes <chrys.dev21@gmail.com>
2025-06-05 19:30:27 -0300

    create aula18 and tested debugger

---
+++ aula17.py
@@ -0,0 +1,23 @@
+# if / elif      / else
+# se / se não se / se não
+
+condition1 = False
+condition2 = False
+condition3 = True
+condition4 = True
+
+if condition1:
+    print("Code for condition 1")
+elif condition2:
+    print("Code for condition 2")
+elif condition3:
+    print("Code for condition 3")
+elif condition4:
+    print("Code for condition 4")
+else:
+    print("No condition was met")
+
+if 10 == 10:
+    print("Another if")
+
+print("Outside the if")
