63eefe4fc9e765290b4b77e966540c4c24cd7818
Chrystian Menezes <chrys.dev21@gmail.com>
2025-06-05 18:59:19 -0300

    Create aula16.py

---
+++ aula16.py
@@ -0,0 +1,16 @@
+# if / elif      / else
+# se / se não se / se não
+
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == "entrar":
+    print("entrando...")
+    print("Você está dentro!!")
+elif entrada == "sair":
+    print("saindo...")
+    print("Você saiu, espero que não volte mais!!")
+else:
+    print("digite corretamente seu inergumino!!")
+
+
+print("você não está mais no bloco.")
