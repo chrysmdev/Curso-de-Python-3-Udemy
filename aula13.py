5f11879897a55c75ef15dc16dcdc0293e6face39
Chrystian Menezes <chrys.dev21@gmail.com>
2025-06-05 15:57:13 -0300

    update aula13 and create aula14

--- aula13.py
+++ aula13.py
@@ -3,8 +3,11 @@ altura = 1.78
 peso = 73
 imc = peso / (altura * altura)
 
+# f-strings
+
 linha_1 = f"{nome} tem {altura:.2f} de altura"
 linha_2 = f"pesa {peso} kilos e tem seu IMC de {imc:.2f}"
+
 print(linha_1)
 print(linha_2)
 
---
+++ aula14.py
@@ -0,0 +1,11 @@
+a = "A"
+b = "B"
+c = 1.1
+string = "a={} b={} c={:.2f}"
+string = "b={1} a={0} a={0} b={1} c={2:.2f}"
+formato = string.format(a, b, c)
+
+string = "b={nome2} a={nome1} a={nome1} b={nome2} c={nome3:.2f}"
+formato = string.format(nome1=a, nome2=b, nome3=c)
+
+print(formato)
