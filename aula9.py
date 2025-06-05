3e10974a08f69606734b4ad52eeb99340a55084a
Chrystian Menezes <chrys.dev21@gmail.com>
2025-06-05 15:18:17 -0300

    Create aula9.py

---
+++ aula9.py
@@ -0,0 +1,23 @@
+adicao = 10 + 10
+print("Adição", adicao)
+
+subtracao = 10 - 5
+print("Subtração", subtracao)
+
+multiplicacao = 10 * 10
+print("Multiplicação", multiplicacao)
+
+divisao = 10 / 2.2  # float
+print("Divisão", divisao)
+
+divisao_inteira = 10 // 2.2
+print("Divisão inteira", divisao_inteira)
+
+exponenciacao = 2**10
+print("Exponenciação", exponenciacao)
+
+modulo = 55 % 2  # resto da divisão
+print("Módulo", modulo)
+
+# se o resto de um número dividido por 2 for igual a 0 ele é um número par, se restar qualquer outro valor ele será impar.
+print(16 % 2 == 0)
