4a2f9dbf2b0de81afd361676b4c937c77cc65395
Chrystian Menezes <chrys.dev21@gmail.com>
2025-06-05 15:41:14 -0300

    Create aula12.py

---
+++ aula12.py
@@ -0,0 +1,11 @@
+nome = "Chrystian Menezes"
+altura = 1.78
+peso = 73
+imc = peso / (altura * altura)
+
+# Chrystian Menezes tem 1.78 de altura,
+# pesa 73 kilos e seu IMC é
+# 23.04002019946976
+
+print(nome, "tem", altura, "de altura,")
+print("pesa", peso, "kilos e tem seu IMC de", imc)
