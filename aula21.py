c1a0bd60b3c896fc722b9140ef3e3bee9ff27d86
Chrystian Menezes <chrys.dev21@gmail.com>
2025-06-08 22:24:59 -0300

    Create aula21.py

---
+++ aula21.py
@@ -0,0 +1,7 @@
+# Logical Operators
+# and - or - not
+# and - All conditions need to be true
+# if any value is considered false, the entire expression will be evalueted at that value
+# They are considered false (which you have already seen)
+# 0.0.0 '' False
+# There is also the type None which is used to represent a non-value
