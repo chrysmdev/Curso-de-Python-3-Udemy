7c9c31bd6f23a473d631db60ef2154cbc65135be
Chrystian Menezes <chrys.dev21@gmail.com>
2025-06-08 22:17:40 -0300

    Update aula20.py

--- aula20.py
+++ aula20.py
@@ -2,8 +2,10 @@ first_value = input("Enter a value: ")
 second_value = input("Enter another value: ")
 
 if first_value > second_value:
-    print(f"{first_value=} is greater than {second_value=}")
-elif second_value > first_value:
-    print(f"{second_value=} is greater than {first_value=}")
+    print(f"{first_value} is greater than {second_value}")
+elif first_value < second_value:
+    print(f"{second_value} is greater than {first_value}")
+elif first_value == second_value:
+    print(f"{first_value} and {second_value} are egual!!")
 else:
     print(f"{first_value=} and {second_value=} cannot be compared")
