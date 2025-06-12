de7ea134551a80f2d68d426312ae000d6cd13e82
Chrystian Menezes <chrys.dev21@gmail.com>
2025-06-11 22:21:14 -0300

    Create aula30.py

---
+++ aula30.py
@@ -0,0 +1,12 @@
+"""
+CONSTANT = "Variables" that do not change
+Too many conditions in the same if (bad)
+    <- Complexity count (bad)
+"""
+
+speed = 61  # current speed of the car
+car_local = 90  # location where the car is on the road
+
+RADAR_1 = 60  # maximum speed of radar 1
+LOCAL_1 = 100  # location where the radar 1
+RADAR_RANGE = 1  # The distance where the radar picks up
