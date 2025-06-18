161ed0b2190d40a5372dcb42baf436bf6a4585e0
Chrystian Menezes <chrys.dev21@gmail.com>
2025-06-18 18:28:30 -0300

    Create aula35.py

---
+++ aula35.py
@@ -0,0 +1,15 @@
+"""
+Repetitions
+while
+performs an action while the condition is true
+infinite loop -> when a code has no end
+"""
+
+counter = 0
+
+while counter <= 10:
+    print(f"your counter is {counter}")
+    counter += 1
+
+print(f"Your counter now is {counter}")
+print("out")
